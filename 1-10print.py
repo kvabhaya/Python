447f24cd8dea568c390155c2ba2fc233fedaa138
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-06-24 15:16:06 +0530

    Update 1-10print.py

--- 1-10print.py
+++ 1-10print.py
@@ -1,6 +1,6 @@
 no=1
-
+print ("no = ",no, " cond-no <= 10 ----- ",no<=10)
 while no<=10:
-  print ("no = ",no, " cond-no <= 10 ----- ",no<=10)
   print(no)
   no=no+1
+  print ("no = ",no, " cond-no <= 10 ----- ",no<=10)
